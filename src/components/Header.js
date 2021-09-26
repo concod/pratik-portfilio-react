3da7e0bfdad0cbb3d189f45b40d48c9751041d4b
Pratik Vermun <pratik.vermun@sevenlakes.com>
2021-09-26 20:50:12 +0530

    updated resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
+import { Row, Col } from "antd";
+
 import { ThemeContext } from "../contexts/ThemeContext";
 import Projects from "./Projects";
 import "./Header.css";
-import { Row, Col } from "antd";
 
 const Header = () => {
   const { state, dispatch } = useContext(ThemeContext);
@@ -49,31 +50,30 @@ const Header = () => {
             }}
           >
             <p>
-              Meticulous and motivated web developer with a flair for creating
-              elegant solutions in the least amount of time. Developed an E-chat
-              webapp,an activity logger for different TZs,a Netflix-clone app,
-              and a Github profile card using React as a front-end tool and
-              express/firebase for managing back-end.
+              3+ years of UI / Front End experience in the new web technologies in the front - end side, experience encompassing various facets of web-based application
+              development highly adaptable and good team player, well organized, self-starter with technical and interpersonal skills.
             </p>
             <p>
-              Developed 4 landing pages,2 DOM games and a portfolio website
-              using vanilla Javascript and CSS. All of my static projects are
-              deployed on Netlify.
+              Experience in working in a Fast paced Agile (Scrum) Development Team to deliver regular updates to business team and project managers
             </p>
             <p>
-              Currently working on a project which requires a complete overhaul
-              of the UI.Using Next.JS as a frontend SSR tool and Strapi(headless
-              CMS) for content management.
+              Strong focus on Responsive Web Design, compliance with W3C Web Standards, applying best practices and leveraging web development by using
+              modern UI/Front-End libraries, frameworks and tools such as HTML4/5, CSS2/3, JavaScript, ReactJs.
             </p>
             <p>
-              Passionate about responsive and elegant web design. Enjoys
-              developing scalable web applications with cutting edge
-              technologies.
+              Currently working on project
+              <a
+                className="header__link"
+                style={{ color: "grey" }}
+                href="https://app.joyn.ai"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                JOYNAI
+              </a> at Seven Lakes Technologies.
             </p>
             <p>
-              This portfolio webapp has been created using modern react tools.I
-              have used context api and reducers for managing themes and
-              states.The source code can be found{" "}
+              This portfolio webapp has been created using modern react tools.The source code can be found{" "}
               <a
                 className="header__link"
                 style={{ color: "grey" }}
